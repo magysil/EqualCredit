0c1cb797d0e40a367d82309cab81e301f1af6db3
ivvnv <ivana.rivera.c@gmail.com>
2020-08-22 11:18:03 -0400

    Vista quienes somos

--- src/views/about.js
+++ src/views/about.js
@@ -1,6 +1,71 @@
 export default () => {
   const viewsAbout = `
-    <h5>Aqui va la informacion de que hacemos y testimonios</h5>
+  <div class="row">
+    <div class="col-sm about-text">
+    <i class="fas fa-users" id="icono"></i>
+    <div class="text-center" style="margin-top:22px;">Somos una aplicación web que otorga créditos automatizados, solo tomando datos que realmente importan para poder brindarle oportunidad a todos por igual.</div>
+    </div>
+    
+    <div class="col-sm about-text">
+    <i class="fab fa-font-awesome-flag" id="icono"></i>
+    <div class="text-center" style="margin-top:22px;">Tenemos como misión darles la oportunidad a todos de surgir, brindar beneficios sin tener ningún prejuicio, ofreciendo un sistema financiero igualitario.</div>
+    </div>
+
+    <div class="col-sm about-text">
+    <i class="fas fa-hands-helping" id="icono"></i>
+    <div class="text-center" style="margin-top:22px;">Los valores de EqualCredit están fundados en la igualdad de oportunidades, en la inclusión y  empatía, en la cooperación, justicia, respeto, solidaridad y tolerancia.</div>
+    </div>
+</div>
+
+<div class="row">
+<div class="col-md-4" style="margin:50px 0 20px 0;"><p class="testim-title">Testimonios Exitosos</p></div>
+</div>
+
+<div class="row">
+
+<div class="col-sm">
+        <div class="card">
+            <div class="card-img-top">
+                <img class="img-fluid" src="img/testi1.jpeg" alt=""></div>
+            <div class="card-block about-text" style="border:1px solid #cecece;">
+                <p class="card-text">Gracias a EqualCredit obtuve un crédito que me permitió crecer económicamente ya que pude invertir y realizar mi emprendimiento.</p>
+                <p><i>María Teresa Parra, 41 años.</i></p>
+                    <a class="btn btn-primary button_quote">
+                    <i class="fas fa-quote-left"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm">
+    <div class="card">
+        <div class="card-img-top">
+            <img class="img-fluid" src="img/testi2.jpeg" alt=""></div>
+            <div class="card-block about-text" style="border:1px solid #cecece;">
+            <p class="card-text">Con la oportunidad que me dio EqualCredit pude crear mi propio negocio, y con eso sentirme realizada como mujer y mamá.</p>
+            <p><i>Carmen Pérez, 35 años.</i></p>
+            <a class="btn btn-primary button_quote">
+            <i class="fas fa-quote-left"></i></a>
+        </div>
+    </div>
+</div>
+
+    <div class="col-sm">
+        <div class="card">
+            <div class="card-img-top">
+                <img class="img-fluid" src="img/testi3.jpeg" alt=""></div>
+            <div class="card-block about-text" style="border:1px solid #cecece;">
+            <p class="card-text">Por ser joven y mujer me cerraron algunas puertas, pero gracias a EqualCredit me dieron esa facilidad sin cuestionar mi capacidad de pago.</p>
+            <p><i>Constanza Peña, 25 años.</i></p>
+                    <a class="btn btn-primary button_quote">
+                    <i class="fas fa-quote-left"></i></a>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
     `;
   const divElement = document.createElement("div");
   divElement.innerHTML = viewsAbout;
--- src/views/home.js
+++ src/views/home.js
@@ -13,6 +13,19 @@ export default () => {
   MÁS INNOVADOR DEL MERCADO, DONDE TODOS CUENTAN</p>
   </header>
 
+  <!-- <div class="container-fluid">
+    <div class="row">
+      <div class="relative-position">
+        <img class="img-responsive" src="img/home_image.jpeg" alt="" />
+        <div class="container absolute-text">
+          <div class="row">
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> -->
+
 
 
   <div class="container">
@@ -27,15 +40,15 @@ export default () => {
     </div>
 
    
-    <div class="py-5 bg-image-full" style="background-image: url('img/home_image.jpeg');">
+  <div class="py-5 bg-image-full" style="background-image: url('img/home_image.jpeg');">
     <a href="#/credit" class="btn btn-info button_class">Pide tu crédito</a>
     <div style="height: 200px;"></div>
-  </div>
+  </div> 
 
 
-  <section id="gallery" class="row no-gutters">
+  <!-- <section id="gallery" class="row no-gutters">
 
-  <!-- <div class="container-home-image" style="margin-top:100px; padding-bottom:50px;">
+<div class="container-home-image" style="margin-top:100px; padding-bottom:50px;">
       <div class="image-widescreen1">
         <img src="img/home_image.jpeg" class="rounded mx-auto d-block"/>
         <a href="#/credit" class="btn btn-info button_class">Pide tu crédito</a>
