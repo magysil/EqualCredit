aae8c6a4f6f557ef32b835dc7ce6c1bd4cc9707e
Magally <magallysilvab@gmail.com>
2020-08-22 06:24:20 -0400

    Vista Contacto

--- src/views/contact.js
+++ src/views/contact.js
@@ -1,6 +1,17 @@
 export default () => {
-  const viewsContact = `
-  <h5>Aqui va la informacion de contactos</h5>
+  const viewsContact = /*html*/ `
+  
+  <div class="text-center">
+    <img class="rounded mx-auto d-block mb-3" width="145" src="./img/logoContacto.png"/>
+    <h5 class='mb-1'>Servicio al Cliente</h5>
+    <h6 class='mb-1'>Teléfono</h6>
+    <h6 class='mb-1'>600 638 6380</h6>
+    <h6 class='mb-1'>Desde celulares</h6>
+    <h6 class='mb-1'>+56 2 26612700</h6>
+    <h6 class='mb-1'>Escríbenos</h6>
+    <h6 class='mb-1'>Horario de Atención y días Contacto Telefónico:</h6>
+    <h6 class='mb-1'>Lunes a domingo de 8:00 a 22:00 hrs.</h6>
+  </div>
     `;
   const divElement = document.createElement("div");
   divElement.innerHTML = viewsContact;
