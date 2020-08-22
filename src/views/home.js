52669aba18c7f12dafd3cfe3be6ee0d373fc4355
ivvnv <ivana.rivera.c@gmail.com>
2020-08-21 23:10:53 -0400

    espacios detalles menores

--- src/views/home.js
+++ src/views/home.js
@@ -1,6 +1,16 @@
 export default () => {
   const viewsHome = `
-  <h5>Aqui va la informacion de home</h5>
+  <div class="container">
+        <div class="row">
+            <div class="col" id="text-style">
+                EqualCredit te permite acceder a créditos de manera igualitaria; evaluamos tus datos reales sin importar quién eres y de manera automática te indicaremos si puedes optar a este beneficio que permitirá abrirte puertas para todo lo que deseas.
+            </div>
+            <div class="col">
+                VIDEO
+            </div>
+        </div>
+        
+    </div>
   
     `;
     
