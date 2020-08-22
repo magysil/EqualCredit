86e3205c2515f7a2762d1713d9086c5cb53f33ff
Magally <magallysilvab@gmail.com>
2020-08-22 15:19:51 -0400

    update

--- src/views/approvedCredit.js
+++
@@ -1,8 +0,0 @@
-export default () => {
-    const viewsNotFound = `
-      <h1>Credito aprobado</h1>
-      `;
-    const divElement = document.createElement("div");
-    divElement.innerHTML = viewsNotFound;
-    return divElement;
-  };
